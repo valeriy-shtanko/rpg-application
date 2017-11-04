1fd33a0bae37de7a25b02d64d591f03d0fb00bc7
Valeriy Shtanko <valeriy.shtanko@gmail.com>
2017-11-05 00:37:18 +0200

    Initial commit.

---
+++ src/main/java/com/rpg/app/App.java
@@ -0,0 +1,11 @@
+package com.rpg.app;
+
+/**
+ * Created by Valeriy Shtanko on 2017-Nov-05, 00:18
+ */
+public class App {
+
+    public static void main( String[] args ) {
+        System.out.println( "Role Playing Game Application starts here." );
+    }
+}
---
+++ src/test/java/com/rpg/app/AppTest.java
@@ -0,0 +1,7 @@
+package com.rpg.app;
+
+/**
+ * Created by Valeriy Shtanko on 2017-Nov-05, 00:16
+ */
+public class AppTest {
+}
